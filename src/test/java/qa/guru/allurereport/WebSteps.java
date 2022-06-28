9d600dfec9135adfe5bd599cc64ee42154f393cd
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-06-28 20:44:13 +0300

    test

--- src/test/java/qa/guru/allurereport/WebSteps.java
+++ src/test/java/qa/guru/allurereport/WebSteps.java
@@ -1,6 +1,7 @@
 package qa.guru.allurereport;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,24 +11,29 @@ import static org.openqa.selenium.By.partialLinkText;
 
 public class WebSteps {
 
+    @Step("Открываем главую страницу")
     public void openMainPage() {
         open("https://github.com/");
     }
 
+    @Step("Ищем репозиторий по имени {repository}")
     public void searchForRepository() {
         $(".header-search-input").click();
         $(".header-search-input").sendKeys("Mihail96tramontana/AllureReport_Ten");
         $(".header-search-input").submit();
     }
 
+    @Step("В результатах поиска переходим по ссылке репозитория {repository}")
     public void openRepositoryLink() {
         $(linkText("Mihail96tramontana/AllureReport_Ten")).click();
     }
 
+    @Step("Открываем таб Issues")
     public void openIssueTab() {
         $(partialLinkText("Issues")).click();
     }
 
+    @Step("Проверяем, что существует Issues с номером {issueNumber}")
     public void shouldSeeIssueWithNumber() {
         $(withText("#1")).should(Condition.exist);
     }
